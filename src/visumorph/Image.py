3ab1c729e8748c2275d61c3ae5c1f57e47a05054
Orix Au Yeung <solosynth1@gmail.com>
2024-01-14 07:54:08 +0800

    20 organizerefactor functions (#27)
    * feat: add skeleton `Image` class and `load_image` function
    
    * refactor: rename `flipping.py` to `flip.py`

---
+++ src/visumorph/Image.py
@@ -0,0 +1,19 @@
+class Image:
+    """The main class for VisuMorph Image objects."""
+    pass
+
+
+def load_image(image_path):
+    """Load an image from filesystem and return an Image object.
+
+    Parameters
+    ----------
+    image_path : str
+        The path to the image file to be read.
+
+    Returns
+    -------
+    image
+        The VisuMorph Image object with image data loaded in.
+    """
+    pass
