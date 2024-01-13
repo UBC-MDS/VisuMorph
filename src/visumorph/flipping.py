ab1952abb34c9f0fd5c3c16583398b38caa3a580
Marcony1 <77180162+Marcony1@users.noreply.github.com>
2024-01-12 17:47:13 -0800

    Flipping function (#15)
    * Created base structure of package
    
    * src: added flipping module with flip function documentation
    
    * flipping.py: output changed to numpy array
    
    * df changed to ndarray
    
    Co-authored-by: Orix Au Yeung <solosynth1@gmail.com>
    
    ---------
    
    Co-authored-by: SoloSynth1 <solosynth1@gmail.com>

---
+++ src/visumorph/flipping.py
@@ -0,0 +1,19 @@
+def flip(input_file, v=0):
+    """Load image from an image file and return as an image numpy array.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to image file.
+    v : int, optional
+        Flip vertically if v=1, horizontally if v=0. By default, v=0.
+
+    Returns
+    -------
+    ndarray
+        Flipped image numpy array.
+
+    Examples
+    --------
+    >>> flip("exampleimage.jpg", v=1)
+    """
