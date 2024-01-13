6e80480ff193902113a9de6dbb1a7a269b32cf94
Atabak Alishiri <143530834+atabak-alishiri@users.noreply.github.com>
2024-01-13 13:07:15 -0800

    Update src/visumorph/change_hue.py
    Co-authored-by: Orix Au Yeung <solosynth1@gmail.com>

--- src/visumorph/change_hue.py
+++ src/visumorph/change_hue.py
@@ -21,7 +21,8 @@ def change_hue(image, delta_hue):
     ValueError: If delta_hue is not a float.
     TypeError
         If the input image is not a valid Image Object.
-    InvalidColorFormatError: If the input image is not in a color format recognized by the VisuMorph package.
+    InvalidColorFormatError
+        If the input image is not in a color format recognized by the VisuMorph package.
 
     Example:
     >>> from VisuMorph import change_hue, load_image
