728591a50c5650f6834442dc1a3580f9a59a60a0
Atabak-Alishiri <alishiriatabak@gmail.com>
2024-01-20 11:43:10 -0800

    changed the function

--- src/visumorph/change_hue.py
+++ src/visumorph/change_hue.py
@@ -1,46 +1,22 @@
+from visumorph import Image as VImage
+from visumorph import load_image
+import numpy as np
 from PIL import Image as PImage
-import click
 
 
-@click.command()
-@click.argument(
-    "image_path", type=click.Path(exists=True), default="tests/img/raw/meme.jpg"
-)
-@click.argument("color", type=str, default="red")
-@click.argument("delta_hue", type=float, default=0.5)
-@click.argument("output_name", type=str, default="hue_adjusted_image")
-def change_hue(
-    image_path, color="white", delta_hue=0.5, output_name="hue_adjusted_image"
-):
-    """
-    Applies a color hue filter to an image and saves the result.
+def change_hue(image, color="white", delta_hue=0.5):
 
-    This function opens an image from the specified path, applies a color layer with a specified transparency,
-    and saves the resulting image. The color layer's hue and transparency can be adjusted.
-
-    Parameters
-    ----------
-    image_path : str
-        The file path to the image to which the hue change will be applied. The path should point to a valid image file.
-    color : str, optional
-        The color to be used for the hue filter. Default is 'white'.
-    delta_hue : float, optional
-        The transparency level for the hue filter layer. Values should be between 0 and 1, with 0 being fully transparent and 1 fully opaque. Default is 0.5.
-    output_name : str, optional
-        The name for the output file. The resulting image will be saved in the 'tests/img/results/' directory with this name and a '.png' extension. Default is 'adjusted_image'.
-
-    Returns
-    -------
-    None
-        This function does not return a value but saves the adjusted image to a file and prints the file path.
-    """
-    im = PImage.open(image_path)
+    if type(image) != VImage:
+        raise TypeError("The image is not a valid VisuMorph Image object")
+    
+    im = PImage.fromarray(image)
     layer = PImage.new("RGB", im.size, color)
     adjusted_image = PImage.blend(im, layer, delta_hue)
-    output_dir = "tests/img/results/" + output_name + ".png"
-    adjusted_image.save(output_dir, "PNG")
-    click.echo(f"Image saved to {output_dir}")
+    adjusted_image_np = np.array(adjusted_image)
 
+    return VImage(adjusted_image_np)
 
-if __name__ == "__main__":
-    change_hue()
+my_image = load_image('tests/img/raw/meme.jpg')
+changed_img = change_hue(my_image)
+to_show = PImage.fromarray(changed_img)
+to_show.save("tests/img/results/new_adjusted.png", "PNG")
