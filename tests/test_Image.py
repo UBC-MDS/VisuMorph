2bca4d20eb07f4b013b3a704ce51c3b80807b75a
SoloSynth1 <solosynth1@gmail.com>
2024-02-03 12:18:09 -0800

    add tests for Image's wrapper methods

--- tests/test_Image.py
+++ tests/test_Image.py
@@ -1,4 +1,4 @@
-from visumorph import load_image
+from visumorph import load_image, scale, rotate, flip, change_hue
 from PIL import Image as PImage
 import numpy as np
 
@@ -25,3 +25,26 @@ def test_image_can_print_out_repr_correctly():
     img = load_image("tests/img/raw/meme.jpg")
     msg = str(img)
     assert msg == "VisuMorph Image, dimensions: (414, 552, 3)"
+
+
+def test_scale_wrapper_should_function_the_same_as_original_func():
+    img = load_image("tests/img/raw/meme.jpg")
+    assert np.array_equal(scale(img, 0.5).image, img.scale(0.5).image)
+
+
+def test_rotate_wrapper_should_function_the_same_as_original_func():
+    img = load_image("tests/img/raw/meme.jpg")
+    assert np.array_equal(rotate(img, 2348.5).image, img.rotate(2348.5).image)
+
+
+def test_flip_wrapper_should_function_the_same_as_original_func():
+    img = load_image("tests/img/raw/meme.jpg")
+    assert np.array_equal(flip(img, 1).image, img.flip(1).image)
+
+
+def test_change_hue_wrapper_should_function_the_same_as_original_func():
+    img = load_image("tests/img/raw/meme.jpg")
+    assert np.array_equal(
+        change_hue(img, color="blue", delta_hue=0.5).image,
+        img.change_hue(color="blue", delta_hue=0.5).image,
+    )
