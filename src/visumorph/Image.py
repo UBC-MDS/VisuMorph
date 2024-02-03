277a9b688b413bb2b689513a60777ea71fb1c0b0
SoloSynth1 <solosynth1@gmail.com>
2024-02-03 12:10:32 -0800

    feat: add wrapper method for scale, flip, rotate, and change_hue for `Image`

--- src/visumorph/Image.py
+++ src/visumorph/Image.py
@@ -1,5 +1,7 @@
 from PIL import Image as PImage
+import visumorph as vm
 import numpy as np
+from functools import partial
 
 
 class Image:
@@ -25,6 +27,139 @@ class Image:
 
     def __repr__(self):
         return f"VisuMorph Image, dimensions: {self.get_dimensions()}"
+
+    def __self_inject(self, func):
+        """Wrapper method to self-inject into visumorph functions."""
+        return partial(func, self)
+
+    def flip(self, *args, **kwargs):
+        """Flip image vertically or horizontally.
+
+        Parameters
+        ----------
+        v : int, optional
+            Flip vertically if v=1, horizontally if v=0. By default, v=0.
+
+        Returns
+        -------
+        image
+            Flipped VisuMorph image.
+
+        Raises
+        ------
+        TypeError
+            If the image is not a valid VisuMorph Image and/or v is not 0 or 1.
+
+        Examples
+        --------
+        >>> import visumorph
+        >>> from visumorph.flip import flip
+        >>> img = visumorph.load_image("exampleimage.jpg")
+        >>> img.flip(v=1)
+        """
+        return self.__self_inject(vm.flip)(*args, **kwargs)
+
+    def rotate(self, *args, **kwargs):
+        """Rotate an image with a given rotation.
+
+        This method will rotate a VisuMorph image by a given degree of rotation.
+        A background image can be specified for filling any empty spaces which
+        are not covered by the image after rotation.
+
+        Parameters
+        ----------
+        rotation : float
+            The desired rotation angle in degrees.
+            +ve means clockwise while -ve means counter-clockwise.
+        background : visumorph.Image, optional
+            The background image to be filled in where the rotated image is not
+            covered. Default is None, which a black background image would be used.
+
+        Returns
+        -------
+        image
+            The VisuMorph Image after rotation.
+
+        Raises
+        ------
+        TypeError
+            If the image is not a valid VisuMorph Image and/or rotation given is
+            not a number.
+
+        Examples
+        --------
+        >>> import visumorph as vm
+        >>> img = vm.load_image("test.jpg")
+        >>> rotated_45_img = img.rotate(img, 45.0)
+        """
+        return self.__self_inject(vm.rotate)(*args, **kwargs)
+
+    def scale(self, *args, **kwargs):
+        """Enlarge/Shrink an image with a given scale.
+
+        This method will scale a VisuMorph image by a given scale with 1.0 as
+        the original scale. The final transformed image would have a dimension of
+        (scale * original_width, scale * original_height), rounded top to the
+        nearest integer.
+
+        Parameters
+        ----------
+        scale : float or int
+            The desired scale. The original scale of the image is 1.0.
+            A scale larger than 1.0 will result in a final image having a larger
+            dimension, and vice versa. Cannot be equal or smaller than 0.0.
+
+        Returns
+        -------
+        image
+            The VisuMorph Image scaled.
+
+        Raises
+        ------
+        TypeError
+            If the image is not a valid VisuMorph Image and/or scale given is
+            not a number.
+        ValueError
+            If the scale provided is equal 0 or is a negative number.
+
+        Examples
+        --------
+        >>> import visumorph as vm
+        >>> img = vm.load_image("test.jpg")
+        >>> scaled_img = img.scale(img, 1.15)
+        """
+        return self.__self_inject(vm.scale)(*args, **kwargs)
+
+    def change_hue(self, *args, **kwargs):
+        """Change the hue of a VisuMorph Image object by blending it with a layer of specified color.
+
+        Parameters
+        ----------
+        color : str, optional
+            The color used for the blending layer (default is "white").
+        delta_hue : float or int, optional
+            The degree of blending with the color layer. A value of 0 means no change,
+            and 1 means complete replacement with the color layer (default is 1).
+
+        Returns
+        -------
+        Image : visumorph.Image
+            A new VisuMorph Image object with the modified hue.
+
+        Raises
+        ------
+        TypeError
+            If the input is not a valid VisuMorph Image object.
+        ValueError
+            If 'delta_hue' is not a number.
+
+        Examples
+        --------
+        >>> import visumorph as vm
+        >>> img = vm.load_image("test.jpg")
+        >>> hue_changed = img.change_hue(img)
+        """
+        return self.__self_inject(vm.change_hue)(*args, **kwargs)
 
 
 def load_image(image_path):
